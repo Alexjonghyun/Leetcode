27494d25c99e0f8fc4248e303851cbf81c4efb72
Alexjonghyun <88361017+Alexjonghyun@users.noreply.github.com>
2022-06-26 12:57:35 +0900

    Time: 3 ms (57.14%), Space: 42.1 MB (40.15%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -4,10 +4,7 @@ class Solution {
     public boolean isValid(String s) {
         Deque<Character> st = new ArrayDeque<>();
         for (int i = 0; i < s.length(); i++) {
-            if(st.isEmpty()) {
-                if(s.charAt(i)==']'||s.charAt(i)==')'||s.charAt(i)=='}') return false;
-                st.addLast(s.charAt(i));
-            }
+            if(st.isEmpty()) st.addLast(s.charAt(i));
             else if(st.peekLast()==s.charAt(i)-1||st.peekLast()==s.charAt(i)-2) {
                 st.pollLast();
                 continue;
